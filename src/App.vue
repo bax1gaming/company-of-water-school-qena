59903295e1eb968eb4e2fa596ccee6b9377c52bf
bax1gaming <msaljhgarhhs@gmail.com>
2025-07-19 09:54:04 +0300

    Fix auth store method name in App.vue

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ import { useAuthStore } from './stores/auth'
 const authStore = useAuthStore()
 
 onMounted(async () => {
-  await authStore.initialize()
+  await authStore.initAuth()
 })
 </script>
 
